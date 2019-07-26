853bdde800251f4418f35edcb450bce397b2491c
Duy Khang <khangemail22@gmail.com>
2019-07-26 21:26:07 +0700

    virtualjoystick into nipplejs

--- public/main.js
+++ public/main.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
   var socket = io();
 
+  //
+  // code của thanh trượt + nút bấm
+  //
+
   $('#slider-range').on('input change', () => {
     $('#angle').html($('#slider-range').val());
   });
@@ -28,5 +32,34 @@ $(document).ready(() => {
   $('#btn-4').click(() => {
     let text = $('#cmd-4').val();
     socket.emit('gui-lenh', {pass: $('#pass').val(), value: {lenh: 'guilenh', giatri: text}});
+  });
+
+  //
+  // code cho joystick, dung nipplejs:
+  //
+
+  var joystick = nipplejs.create({
+        zone: document.getElementById('touchpad'),
+        color: 'blue',
+        size: 200,
+  });
+
+  var tocdo = ['đứng yên', 'chầm chậm', 'vừa vừa', 'nhanh!']
+  $('#speed-range').on('input change', () => {
+    $('#speed').html(tocdo[$('#speed-range').val()]);
+  });
+
+  var x, y;
+  joystick.on('move', (event, nipple) => {
+    // console.log(nipple.instance.frontPosition);
+    x = Math.round(nipple.instance.frontPosition.x);
+    y = Math.round(nipple.instance.frontPosition.y) * (-1);
+  }).on('end', (event, nipple) => {
+    x = 0;
+    y = 0;
+  }).on('move end', (event, nipple) => {
+    $('#x').html(x);
+    $('#y').html(y);
+    socket.emit('gui-lenh', {pass: $('#pass').val(), value: {lenh: 'joystick', giatri: {x: x, y: y, tocdo: parseInt($('#speed-range').val())}}});
   });
 })
