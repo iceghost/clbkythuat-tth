d646121d6e4a4638afb75b75f928d910f382f60b
Duy Khang <khangemail22@gmail.com>
2019-09-14 17:35:51 +0700

    Added FB Access Code

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const ip = require('ip');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 app.use(express.static("./public"));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
@@ -18,17 +20,56 @@ app.get('/guide', (req, res) => {
   res.render('guide');
 });
 
+// Adds support for GET requests to our webhook
 app.get('/facebook', (req, res) => {
-  let VERIFY_TOKEN = 'iot-bot';
 
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "iot-bot"
+
+  // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
-  if (mode && token === VERIFY_TOKEN) {
-    res.status(200).send(challenge);
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+// Creates the endpoint for our webhook 
+app.post('/facebook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
   } else {
-    res.sendStatus(403);
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
 });
 
