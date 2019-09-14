48b8e802c02f76e786db5eb0be6cec2f853f6129
Duy Khang <khangemail22@gmail.com>
2019-09-14 17:20:05 +0700

    Add FMAPI

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 const ip = require('ip');
@@ -17,18 +18,32 @@ app.get('/guide', (req, res) => {
   res.render('guide');
 });
 
-var login = []; // Danh sách những arduino đăng nhập, login[...].id = id của socket
+app.get('/facebook', (req, res) => {
+  let VERIFY_TOKEN = 'iot-bot';
+
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+var ESPs = []; // Danh sách những arduino đăng nhập, ESPs[...].id = id của socket
 // Nếu pass bị trùng thì hỏng hết :))
 
 io.on("connection", (socket) => {
   // Arduino đăng nhập tại đây
   socket.on('dang-nhap', (data) => {
-    // console.log('Login from ' + socket.id)
-    var index = login.map(value => value.pass).indexOf(data);
+    // console.log('ESPs from ' + socket.id)
+    var index = ESPs.map(value => value.pass).indexOf(data);
     if (index >= 0) {
-      login.splice(index, 1);
+      ESPs.splice(index, 1);
     }
-    login.push({
+    ESPs.push({
       pass: data.toString(),
       id: socket.id,
       log: [],
@@ -37,32 +52,32 @@ io.on("connection", (socket) => {
 
   socket.on('gui-lenh', (data) => {
     console.log(data)
-    var index = login.map(value => value.pass).indexOf(data.pass);
+    var index = ESPs.map(value => value.pass).indexOf(data.pass);
     if (index >= 0) {
-      socket.broadcast.to(login[index].id).emit('gui-lenh', data.value); // gửi đến socket có id trong JSON login
+      socket.broadcast.to(ESPs[index].id).emit('gui-lenh', data.value); // gửi đến socket có id trong JSON ESPs
     }
   });
 
   socket.on('disconnect', () => {
-    var index = login.map(value => value.id).indexOf(socket.id);
+    var index = ESPs.map(value => value.id).indexOf(socket.id);
     if (index >= 0) {
-      login.splice(index, 1);
+      ESPs.splice(index, 1);
     }
   });
 
   socket.on('tim-nguoi-than', (data) => {
-    var index = login.map(value => value.pass).indexOf(data);
+    var index = ESPs.map(value => value.pass).indexOf(data);
     if (index >= 0) {
-      socket.emit('ket-qua', login[index]);
-    } 
+      socket.emit('ket-qua', ESPs[index]);
+    }
     else socket.emit('ket-qua', null);
   });
 
   socket.on('log', (data) => {
-    var index = login.map(value => value.id).indexOf(socket.id); // tìm số thứ tự của esp8266 trong mảng login
+    var index = ESPs.map(value => value.id).indexOf(socket.id); // tìm số thứ tự của esp8266 trong mảng ESPs
     if (index >= 0) {
       var now = new Date();
-      login[index].log.push({time: now, content: data});
+      ESPs[index].log.push({ time: now, content: data });
     }
   });
 });
