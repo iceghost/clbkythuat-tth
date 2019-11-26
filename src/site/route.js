a800262bcbf87559800829f1e7753c85a7743280
Duy Khang <khangemail22@gmail.com>
2019-11-26 16:43:55 +0700

    Added RC Car Control

--- src/site/route.js
+++ src/site/route.js
@@ -4,5 +4,6 @@ const router = Router();
 
 router.get('/', (req, res) => { res.render('site/home'); });
 router.get('/guide', (req, res) => { res.render('site/guide'); });
+router.get('/rc-car', (req, res) => { res.render('site/rc-car'); });
 
 export default router;
